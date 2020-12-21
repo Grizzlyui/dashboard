b77692fe7ac2d1e8f173b786796b832e6c957d35
Grizzlyui <>
2020-12-21 09:48:45 +0200

    test

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const searchForm = document.querySelector("#searchForm");
-const searchBox = document.querySelector('#search-box')
+// const searchForm = document.querySelector("#searchForm");
+// const searchBox = document.querySelector('#search-box')
 
-searchForm.addEventListener('submit', (e) => { 
-    e.preventDefault();    
-    const choice = document.querySelector('input[name="choices"]:checked');     
-    if (choice.value === "google") {     
-        window.location = `https://www.google.com/search?q=${searchBox.value}`; 
-    } 
-    if (choice.value === "youtube") {     
-        window.location = `https://www.youtube.com/results?search_query=${searchBox.value}`; 
-    } 
-    if (choice.value === "reddit") {     
-        window.location = `https://www.reddit.com/search/?q=${searchBox.value}`; 
-    }  
-    if (choice.value === "pinterest") {     
-        window.location = `https://za.pinterest.com/search/pins/?q=${searchBox.value}`; 
-    }     
-    searchBox.value = '';
-});   
+// searchForm.addEventListener('submit', (e) => { 
+//     e.preventDefault();    
+//     const choice = document.querySelector('input[name="choices"]:checked');     
+//     if (choice.value === "google") {     
+//         window.location = `https://www.google.com/search?q=${searchBox.value}`; 
+//     } 
+//     if (choice.value === "youtube") {     
+//         window.location = `https://www.youtube.com/results?search_query=${searchBox.value}`; 
+//     } 
+//     if (choice.value === "reddit") {     
+//         window.location = `https://www.reddit.com/search/?q=${searchBox.value}`; 
+//     }  
+//     if (choice.value === "pinterest") {     
+//         window.location = `https://za.pinterest.com/search/pins/?q=${searchBox.value}`; 
+//     }     
+//     searchBox.value = '';
+// });   
 
 
 
